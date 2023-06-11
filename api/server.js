d2079ed7dcc1c1cb203bd1bfccdf5f2340228c5e
tigerweirdo <temmuzcetiner@gmail.com>
2023-06-11 20:21:18 +0200

    tekrar2

--- api/server.js
+++ api/server.js
@@ -1,14 +1,4 @@
 // server için gerekli olanları burada ayarlayın
-const express = require("express"); 
-const server = express();
-const postRouter = require("./posts/posts-router");
-
-
-server.use(express.json());//json isteklerini desteklemesi için
-
-server.use("/api/posts", postRouter) 
 
 // posts router'ını buraya require edin ve bağlayın
 
-
-module.exports = server;
--- index.js
+++ index.js
@@ -1,8 +1,1 @@
 // require your server and launch it here
-const server = require("./api/server");
-
-const port = 9000
-
-server.listen(port,()=>{
-  console.log( `server is listenin on ${port}` )
-})
