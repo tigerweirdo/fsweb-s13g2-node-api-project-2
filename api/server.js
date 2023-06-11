53c61599ed5139fa383cc3bb53dde0f8d25df595
tigerweirdo <temmuzcetiner@gmail.com>
2023-06-11 14:50:28 +0200

    tekrar1.

--- api/server.js
+++ api/server.js
@@ -1,15 +1,3 @@
 // server için gerekli olanları burada ayarlayın
 
 // posts router'ını buraya require edin ve bağlayın
-const express = require('express');
-const cors = require('cors');
-
-const postsRouter = require('./posts/posts-router.js');
-
-const server = express();
-
-server.use(express.json());
-server.use(cors());
-server.use('/api/posts', postsRouter);
-
-module.exports = server;
